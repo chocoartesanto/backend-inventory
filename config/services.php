ed17e74ebfea3f562c06e03ae57c4102806ef21d
yordansolis <yordansolis2@gmail.com>
2025-07-31 09:10:59 -0500

    agregando archivos5

--- config/services.php
+++ config/services.php
@@ -14,19 +14,19 @@ return [
     |
     */
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+    // 'postmark' => [
+    //     'token' => env('POSTMARK_TOKEN'),
+    // ],
 
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+    // 'ses' => [
+    //     'key' => env('AWS_ACCESS_KEY_ID'),
+    //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    // ],
 
     'slack' => [
         'notifications' => [
